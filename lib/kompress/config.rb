40fda60d85e5d4162aaa0c0080466162a45d3ce0
Erik Kastner <kastner@gmail.com>
2008-09-15 01:05:21 -0400

    more tests - substitutions work now

--- lib/kompress/config.rb
+++ lib/kompress/config.rb
@@ -2,12 +2,10 @@ module Kompress
   module Config
     extend self
     
+    attr_reader :commands, :settings
+    
     def presets
       @presets || {}
-    end
-    
-    def commands(command)
-      @commands[command]
     end
     
     def write
@@ -17,6 +15,39 @@ module Kompress
     def command(hash)
       @commands ||= {}
       @commands.merge! hash
+    end
+    
+    def setting(hash)
+      @settings ||= {}
+      @settings.merge! hash
+    end
+    
+    def preset(hash)
+      @presets ||= {}
+      hash.each do |k, v|
+        @presets[k] = Preset.new(k, v)
+      end
+    end
+    
+    class Preset
+      attr_reader :name
+      attr_accessor :options
+      
+      def initialize(name, options = {})
+        @command = options.delete(:command) || ""
+        @name = name.to_s
+        @options = options
+      end
+      
+      def command
+        kc = Kompress::Config
+        replacements = kc.settings.merge(kc.commands.merge(@options))
+        @command.gsub(/:\w+/) {|s| replacements[s.gsub(/^:/,'').intern] || s }
+      end
+      
+      def description
+        @options[:description] || @name
+      end
     end
   end
 end
--- test/config_test.rb
+++ test/config_test.rb
@@ -10,10 +10,38 @@ describe "Kompress::Config" do
   setup do
     Kompress::Config.write do |k|
       k.command :ffmpeg => "/usr/bin/ffmpeg"
+      k.setting :directory => "/t"
+      
+      k.preset :test => {
+        :command => ":ffmpeg go > :directory"
+      }
     end
   end
   
   it "should map commands to locations" do
-    Kompress::Config.commands(:ffmpeg).should == "/usr/bin/ffmpeg"
+    Kompress::Config.commands[:ffmpeg].should == "/usr/bin/ffmpeg"
+  end
+
+  it "should map settings" do
+    Kompress::Config.settings[:directory].should == "/t"
+  end
+  
+  it "should substitue placeholders" do
+    Kompress::Config.presets[:test].command.should == "/usr/bin/ffmpeg go > /t"
+  end
+end
+
+describe "Kompress::Config::Preset" do
+  setup do
+    @p = Kompress::Config::Preset.new(:rad, {:description => "Rad testing preset"})
+  end
+  
+  it "should have a description" do
+    @p.description.should == "Rad testing preset"
+  end
+  
+  it "should use the name as a description if none provided" do
+    @p.options.delete(:description)
+    @p.description.should == "rad"
   end
 end
